edc19f27e08c6511c542e2ce51434ed0c0a01d41
NayaraNicacio <nayaranicacio42@gmail.com>
2024-09-05 13:26:01 +0000

    Adicionado rota post

--- Entrega-Exe-1/server.js
+++ Entrega-Exe-1/server.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const cors = require('cors')
 const router = express.Router()
-const listaLivros = ('./Livros.json')
+const listEndpoints = require("express-list-endpoints")
+const listaLivros = require('./Livros.json')
 
 
 const app = express();
@@ -9,14 +10,32 @@ app.use(express.json());
 app.use(cors())
 const PORT = 3333
 
+const novolivro = []
+
 // rota get
 app.get('/', (req, res) => {
     res.send('Bem-vindo ao servidor Express!');
 });
+ 
 
-const getLivros = router.get('/Livros', (req, res) =>{
-    res.json(listaLivros)
-})
+app.get('/Livros', (req, res) => {
+    res.json(listaLivros);
+});
 
-app.use(getLivros)
-app.listen(console.log("Servidor Rodando na porta ", PORT))
+
+app.post('/novolivro', (req, res) => {
+    const livro = {
+        id: req.body.id,
+        autor: req.body.autor, // Convencionalmente, use letras minúsculas
+        titulo: req.body.titulo
+    };
+
+    novolivro.push(livro); // Adiciona o novo livro à lista
+    res.json(novolivro); // Retorna a lista atualizada
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+    console.log(listEndpoints(app));
+});
